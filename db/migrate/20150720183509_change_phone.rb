6ca1b6e8f7d5dc9f86bffb3121d87a018caa1ed5
Jessica Engel <Jessicaepdx@gmail.com>
2015-07-20 11:58:37 -0700

    address book will list out contacts in table, add contact form will add contact to address book

---
+++ db/migrate/20150720183509_change_phone.rb
@@ -0,0 +1,6 @@
+class ChangePhone < ActiveRecord::Migration
+  def change
+    remove_column :contacts, :phone
+    add_column :contacts, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720175426) do
+ActiveRecord::Schema.define(version: 20150720183509) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string  "name"
-    t.integer "phone"
+    t.string "name"
+    t.string "phone"
   end
 
   create_table "messages", force: :cascade do |t|
