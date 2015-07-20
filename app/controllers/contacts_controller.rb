41250cfdfccfa8d2a2c6021ce785113fd6e4c10b
Jessica Engel <Jessicaepdx@gmail.com>
2015-07-20 12:25:22 -0700

    flash notice will show on form for error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -17,10 +17,16 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:notice]="You added " + @contact.name + " to your Address Book!"
-      redirect_to root_path
+      respond_to do |format|
+        format.html {render 'index'}
+        format.js
+      end
     else
       flash[:notice] = "Oops! Please fill in name and phone number, do not include dashes."
-      render :new
+      respond_to do |format|
+        format.html {render 'index'}
+        format.js {render 'error'}
+      end
     end
   end
 
